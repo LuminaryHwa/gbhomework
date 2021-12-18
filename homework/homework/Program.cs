3fbf5c2ce29589b3dd924ffc36cc048193e63241
Eugene <luminaryhwa@gmail.com>
2021-12-18 17:40:16 +0500

    Finished
    Finished

---
+++ homework/homework/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace homework
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What is your name?");
+
+            var name = Console.ReadLine();
+
+            Console.WriteLine($" Привет, {name}! Сегодня [{DateTime.Now}]");
+        }
+
+    }
+}
