5b2a892a2ca6ce05a209f3f8f3afecad57dbdc0b
Eugene <luminaryhwa@gmail.com>
2021-12-21 00:11:33 +0500

    Finished
    Finished

---
+++ Homework2_1/Homework2_1/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Homework2_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double maxTemp;
+
+            double minTemp;
+
+            Console.WriteLine($"Максимальная температура за сутки");
+
+            maxTemp = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine($"Минимальная температура за сутки");
+
+            minTemp = Convert.ToInt32(Console.ReadLine());
+
+            double srednTemp = minTemp + maxTemp;
+
+            Console.WriteLine("Средняя температура"); 
+            
+            Console.WriteLine(srednTemp / 2);
+
+
+
+
+        }
+    }
+}
